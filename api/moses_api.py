658da3531d1cdb895d2ef15f9c6eb2f745c32b49
miguelemosreverte <miguellemos95@gmail.com>
2017-03-26 21:04:08 -0300

    Before this commit the Language Model was used as a Translation Model target. Now that is a separate file, so that the user can send three different files, one for Translation Model source, one for target, and one for the Language Model.

--- api/moses_api.py
+++ api/moses_api.py
@@ -51,24 +51,28 @@ def uploadCorpus():
     """
 
     LM_FILEPATH = "/home/moses/temp/LanguageModel.txt"
-    TM_FILEPATH = "/home/moses/temp/TranslationModel.txt"
+    TM_source_FILEPATH = "/home/moses/temp/TranslationModel_source.txt"
+    TM_target_FILEPATH = "/home/moses/temp/TranslationModel_target.txt"
 
-    TM = request.form['TM']
+    TM_source = request.form['TM_source']
+    TM_target = request.form['TM_target']
     LM = request.form['LM']
     source_lang = request.form['source_lang']
     target_lang = request.form['target_lang']
     LM_name = request.form['LM_name']
 
-    if (TM and LM and source_lang and target_lang and LM_name):
+    if (TM_source and TM_target and LM and source_lang and target_lang and LM_name):
 
         with open(LM_FILEPATH, "w") as f:
-            f.write(TM.encode('utf-8'))
-        with open(TM_FILEPATH, "w") as f:
             f.write(LM.encode('utf-8'))
+        with open(TM_source_FILEPATH, "w") as f:
+            f.write(TM_source.encode('utf-8'))
+        with open(TM_target_FILEPATH, "w") as f:
+            f.write(TM_target.encode('utf-8'))
 
         newpath = '/home/moses/language_models/' + LM_name
         if not os.path.exists(newpath):
             os.makedirs(newpath)
-        return ttt._prepare_corpus(newpath,source_lang,target_lang,LM_FILEPATH,TM_FILEPATH,TM_FILEPATH)
+        return ttt._prepare_corpus(newpath,source_lang,target_lang,TM_source_FILEPATH,TM_target_FILEPATH,LM_FILEPATH)
     else:
         return ('Error reading file...\n')
